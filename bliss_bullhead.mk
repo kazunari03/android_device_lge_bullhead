4657132df186240382aefa797a90ecfabd11abb2
Eric Park <ideamaneric@gmail.com>
2018-09-17 11:44:15 -0400

    Build fingerprint is out of date

--- bliss_bullhead.mk
+++ bliss_bullhead.mk
@@ -15,10 +15,4 @@ PRODUCT_MODEL := Nexus 5X
 TARGET_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
-# Device Fingerprint
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=bullhead \
-    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM6.171019.030.K1 4947289 release-keys"
-
-BUILD_FINGERPRINT := google/bullhead/bullhead:8.1.0/OPM6.171019.030.K1/4947289:user/release-keys
 
