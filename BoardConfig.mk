a674c3c56cfb6f52c8342dbc2649af8eb69d2ea5
Eric Park <ideamaneric@gmail.com>
2018-11-18 01:23:00 -0500

    kernel: use @milouk's UberTC-8.0.1 toolchain

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,7 @@ TARGET_NO_BOOTLOADER := true
 BUILD_TOP := $(shell pwd)
 
 # Inline kernel building
-KERNEL_TOOLCHAIN := $(BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+KERNEL_TOOLCHAIN := $(BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-8.0.1/bin
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/lge/bullhead
 TARGET_KERNEL_CONFIG := sphinx_defconfig
